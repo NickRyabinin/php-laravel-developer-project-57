90f0cb82f0c5d0d7f57a8f42453f68a8b191cf6b
Николай <rnn2002@mail.ru>
2023-10-27 19:35:40 +0300

    Add page/home view

---
+++ resources/views/page/home.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.app')
+
+@section('header', 'Привет от Хекслета!')
+
+<!-- Секция, содержащая HTML блок. Имеет открывающую и закрывающую часть. -->
+@section('content')
+    <p class="my-4">Это простой менеджер задач на Laravel</p>
+    <a href="https://github.com/NickRyabinin" target="__blank"
+        class="align-middle bg-transparent hover:bg-blue-500 text-blue-700 text-base font-semibold py-1 px-2 hover:text-white border border-blue-500 hover:border-transparent rounded">
+        Нажми меня
+    </a>
+@endsection
