5ff3d5d31cdf65b81d1854a5d6770958afa96279
Николай <rnn2002@mail.ru>
2023-10-28 20:26:25 +0300

    Modify layouts/navigation

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -4,93 +4,48 @@
         <div class="flex justify-between h-16">
             <div class="flex">
                 <div class="shrink-0 flex mr-20">
-                    <x-nav-link :href="route('home')" :active="request()->routeIs('home')">
+                    <x-nav-link :href="route('home')" :active="request()->routeIs('home')" class="hover:text-blue-700">
                         {{ __('Менеджер задач') }}
                     </x-nav-link>
                 </div>
 
                 <!-- Navigation Links -->
-                <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
-                    <x-nav-link :href="route('home')" :active="request()->routeIs('home')">
+                <div class="hidden space-x-8 sm:-my-px sm:ml-10 md:flex">
+                    <x-nav-link :href="route('home')" :active="request()->routeIs('home')" class="hover:text-blue-700">
                         {{ __('Задачи') }}
                     </x-nav-link>
-                    <x-nav-link :href="route('home')" :active="request()->routeIs('home')">
+                    <x-nav-link :href="route('home')" :active="request()->routeIs('home')" class="hover:text-blue-700">
                         {{ __('Статусы') }}
                     </x-nav-link>
-                    <x-nav-link :href="route('home')" :active="request()->routeIs('home')">
+                    <x-nav-link :href="route('home')" :active="request()->routeIs('home')" class="hover:text-blue-700">
                         {{ __('Метки') }}
                     </x-nav-link>
+                </div>
+                <div class="space-x-4 sm:ml-10 absolute top-5 right-14">
                     @guest
-                        <x-nav-link :href="route('home')" :active="request()->routeIs('home')">
+                        <a href="{{ route('login') }}"
+                            class="align-middle bg-transparent hover:bg-blue-500 text-blue-700 text-base font-semibold py-1 px-2 hover:text-white border border-blue-500 hover:border-transparent rounded">
                             {{ __('Вход') }}
-                        </x-nav-link>
-                        <x-nav-link :href="route('home')" :active="request()->routeIs('home')">
+                        </a>
+                        <a href="{{ route('register') }}"
+                            class="align-middle bg-transparent hover:bg-blue-500 text-blue-700 text-base font-semibold py-1 px-2 hover:text-white border border-blue-500 hover:border-transparent rounded">
                             {{ __('Регистрация') }}
-                        </x-nav-link>
+                        </a>
                     @endguest
                     @auth
-                        {{ __('Выход') }}
+                        {{ Form::open(['route' => 'logout', 'method' => 'post', 'class' => 'inline']) }}
+                        {{ Form::button(__('Выход'), [
+                            'type' => 'submit',
+                            'class' =>
+                                'align-middle bg-transparent hover:bg-blue-500 text-blue-700 text-base font-semibold py-0.5 px-2 hover:text-white border border-blue-500 hover:border-transparent rounded',
+                        ]) }}
+                        {{ Form::close() }}
                     @endauth
                 </div>
             </div>
 
-            <!-- Settings Dropdown -->
-            <div class="hidden sm:flex sm:items-center sm:ml-10">
-                <x-dropdown align="right" width="48">
-                    <x-slot name="trigger">
-                        <button
-                            class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-blue-800 bg-white hover:text-gray-700 focus:outline-none transition ease-in-out duration-150">
-                            <div>
-                                @auth
-                                    {{ Auth::user()->name }}
-                                @endauth
-                                @guest
-                                    {{ __('Гость') }}
-                                @endguest
-                            </div>
-
-                            <div class="ml-1">
-                                <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg"
-                                    viewBox="0 0 20 20">
-                                    <path fill-rule="evenodd"
-                                        d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-                                        clip-rule="evenodd" />
-                                </svg>
-                            </div>
-                        </button>
-                    </x-slot>
-
-                    <x-slot name="content">
-                        @auth
-                            <x-dropdown-link :href="route('profile.edit')">
-                                {{ __('Профиль') }}
-                            </x-dropdown-link>
-
-                            <!-- Authentication -->
-                            <form method="POST" action="{{ route('logout') }}">
-                                @csrf
-
-                                <x-dropdown-link :href="route('logout')"
-                                    onclick="event.preventDefault();
-                                                this.closest('form').submit();">
-                                    {{ __('Выход') }}
-                                </x-dropdown-link>
-                            </form>
-                        @endauth
-                        @guest
-                            <x-dropdown-link :href="route('login')">
-                                {{ __('Вход') }}
-                            </x-dropdown-link>
-                            <x-dropdown-link :href="route('register')">
-                                {{ __('Регистрация') }}
-                            </x-dropdown-link>
-                        @endguest
-                    </x-slot>
-                </x-dropdown>
-            </div>
-
             <!-- Hamburger -->
-            <div class="-mr-2 flex items-center sm:hidden">
+            <div class="-mr-2 flex items-center md:hidden">
                 <button @click="open = ! open"
                     class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 focus:text-gray-500 transition duration-150 ease-in-out">
                     <svg class="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
@@ -106,7 +61,7 @@
     </div>
 
     <!-- Responsive Navigation Menu -->
-    <div :class="{ 'block': open, 'hidden': !open }" class="hidden sm:hidden">
+    <div :class="{ 'block': open, 'hidden': !open }" class="hidden md:hidden">
         <div class="pt-2 pb-3 space-y-1">
             <x-responsive-nav-link :href="route('home')" :active="request()->routeIs('home')">
                 {{ __('Задачи') }}
