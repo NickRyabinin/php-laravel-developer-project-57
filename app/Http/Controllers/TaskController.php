10785ca4f0b369a676227e2b78acc1465fc4b49d
Николай <rnn2002@mail.ru>
2023-11-01 20:05:45 +0300

    Modify TaskController

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use App\Models\Task;
+use App\Models\TaskStatus;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class TaskController extends Controller
@@ -23,7 +25,9 @@ class TaskController extends Controller
     {
         if (auth()->check()) {
             $task = new Task();
-            return view('tasks.create', ['task' => $task]);
+            $taskStatuses = TaskStatus::select('id', 'name')->get()->pluck('name', 'id');
+            $users = User::select('id', 'name')->get()->pluck('name', 'id');
+            return view('tasks.create', ['task' => $task, 'taskStatuses' => $taskStatuses, 'users' => $users]);
         }
         abort(403, 'This action is unauthorized.');
     }
@@ -33,7 +37,17 @@ class TaskController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $validated = $request->validate([
+            'name' => 'required|unique:tasks|string|max:255',
+            'description' => 'max:10000',
+            'status_id' => 'required',
+            'assigned_to_id' => '',
+        ]);
+
+        $task = new Task();
+        $task->fill($validated)->fill(['created_by_id' => auth()->user()->id])->save();
+        session()->flash('message', 'Задача успешно создана');
+        return redirect()->route('tasks.index');
     }
 
     /**
@@ -50,7 +64,9 @@ class TaskController extends Controller
     public function edit(Task $task)
     {
         if (auth()->check()) {
-            return view('tasks.edit', ['task' => $task]);
+            $taskStatuses = TaskStatus::select('id', 'name')->get()->pluck('name', 'id');
+            $users = User::select('id', 'name')->get()->pluck('name', 'id');
+            return view('tasks.edit', ['task' => $task, 'taskStatuses' => $taskStatuses, 'users' => $users]);
         }
         abort(403, 'This action is unauthorized.');
     }
@@ -60,7 +76,16 @@ class TaskController extends Controller
      */
     public function update(Request $request, Task $task)
     {
-        //
+        $validated = $request->validate([
+            'name' => 'required|string|max:255|unique:tasks,name,' . $task->id,
+            'description' => 'max:10000',
+            'status_id' => 'required',
+            'assigned_to_id' => '',
+        ]);
+
+        $task->fill($validated)->save();
+        session()->flash('message', 'Задача успешно изменена');
+        return redirect()->route('tasks.index');
     }
 
     /**
@@ -68,9 +93,9 @@ class TaskController extends Controller
      */
     public function destroy(Task $task)
     {
-        $taskStatus->delete();
-            session()->flash('message', 'Задача успешно удалена');
-            // session()->flash('message', 'Не удалось удалить задачу');
-            return redirect()->route('tasks.index');
+        $task->delete();
+        session()->flash('message', 'Задача успешно удалена');
+        // session()->flash('message', 'Не удалось удалить задачу');
+        return redirect()->route('tasks.index');
     }
 }
