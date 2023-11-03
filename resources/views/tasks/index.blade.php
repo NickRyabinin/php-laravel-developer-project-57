edd06beeec01e6546238f22f5cfcb0f0b2c17d30
Николай <rnn2002@mail.ru>
2023-11-03 16:41:23 +0300

    Modify some "tasks" views

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -32,18 +32,18 @@
                     @foreach ($tasks as $task)
                         <tr class="border-b border-dashed text-left">
                             <td>{{ $task->id }}</td>
-                            <td>{{ $task->id }}</td>
+                            <td>{{ $task->taskStatus->name }}</td>
                             <td>
                                 <a href="{{ route('tasks.show', $task) }}"
                                     class="align-middle text-green-500">{{ $task->name }}
                                 </a>
                             </td>
-                            <td>{{ $task->id }}</td>
-                            <td>{{ $task->id }}</td>
+                            <td>{{ $task->creator->name }}</td>
+                            <td>{{ $task->executor->name ?? '' }}</td>
                             <td>{{ $task->created_at->format('d.m.Y') }}</td>
                             <td>
                                 @auth
-                                    @if (auth()->user()->id === $task->created_by_id)
+                                    @if (auth()->user()->id === $task->creator->id)
                                         @include('tasks.delete')
                                     @endif
                                     <a class="text-blue-600 hover:text-blue-900" href="{{ route('tasks.edit', $task) }}">
--- resources/views/tasks/show.blade.php
+++ resources/views/tasks/show.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('header')
-    Просмотр задачи: {{$task->name}}
+    Просмотр задачи: {{ $task->name }}
     <div class="inline">
         <a href="{{ route('tasks.edit', $task) }}">&#9881;</a>
     </div>
@@ -9,7 +9,13 @@
 
 @section('content')
     <p class="mb-2">Имя: {{ $task->name }}</p>
-    <p class="mb-2">Статус: {{ $task->id }}</p>
+    <p class="mb-2">Статус: {{ $task->taskStatus->name }}</p>
     <p class="mb-2">Описание: {{ $task->description }}</p>
-    <p class="mb-2">Метки: {{ $task->id }}</p>
+    <p class="mb-2">Метки:</p>
+    <div>
+        @if (isset($labels))
+            @foreach ($labels as $label)
+            @endforeach
+        @endif
+    </div>
 @endsection
