8b717e32c1de8570cd653f86535550d1b7322bf9
Николай <rnn2002@mail.ru>
2023-11-05 19:36:12 +0300

    Modify LabelTest

--- tests/Feature/LabelTest.php
+++ tests/Feature/LabelTest.php
@@ -45,4 +45,63 @@ class LabelTest extends TestCase
     {
         $response = $this->get(route('labels.create'))->assertStatus(403);
     }
+
+    /**
+     * A 'labels.store' test.
+     */
+    public function test_labels_store_action_can_save_new_label_to_database(): void
+    {
+        $response = $this->actingAs($this->user)
+            ->post(route('labels.store'), ['name' => $this->label->name, 'description' => $this->label->description]);
+        $response->assertSessionHasNoErrors();
+        $this->assertDatabaseHas('labels', ['name' => $this->label->name, 'description' => $this->label->description]);
+        $response->assertRedirect(route('labels.index'));
+        $this->get(route('labels.index'))->assertSee($this->label->name);
+    }
+
+    /**
+     * A 'labels.edit' page accessibility test.
+     */
+    public function test_labels_edit_screen_can_be_rendered_for_authenticated_user(): void
+    {
+        $this->label->save();
+        $response = $this->actingAs($this->user)->get(route('labels.edit', ['label' => $this->label]));
+        $response->assertStatus(200);
+        $response->assertSee('Изменение метки');
+    }
+
+    public function test_labels_edit_screen_can_not_be_rendered_for_guest_user(): void
+    {
+        $this->label->save();
+        $response = $this->get(route('labels.edit', ['label' => $this->label]))->assertStatus(403);
+    }
+
+    /**
+     * A 'labels.update' test.
+     */
+    public function test_labels_update_action_can_patch_existing_label_in_database(): void
+    {
+        $this->label->save();
+        $newLabelData = Label::factory()->make()->only(['name', 'description']);
+        $response = $this->actingAs($this->user)
+            ->patch(route('labels.update', ['label' => $this->label]), $newLabelData);
+        $response->assertSessionHasNoErrors();
+        $this->assertDatabaseHas('labels', $newLabelData);
+        $response->assertRedirect(route('labels.index'));
+        $this->get(route('labels.index'))->assertSee($newLabelData['name']);
+    }
+
+    /**
+     * A 'labels.destroy' test.
+     */
+    public function test_labels_destroy_action_can_delete_existing_label_in_database(): void
+    {
+        $this->label->save();
+        $response = $this->actingAs($this->user)
+            ->delete(route('labels.destroy', ['label' => $this->label]));
+        $response->assertSessionHasNoErrors();
+        $this->assertDatabaseMissing('labels', ['id' => $this->label->id]);
+        $response->assertRedirect(route('labels.index'));
+        $this->get(route('labels.index'))->assertDontSee($this->label->name);
+    }
 }
