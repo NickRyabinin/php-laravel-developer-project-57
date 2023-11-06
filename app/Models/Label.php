747b1fb4d110a903f3b02058b1f946950be6481f
Николай <rnn2002@mail.ru>
2023-11-06 13:31:56 +0300

    Modify "Label" model

--- app/Models/Label.php
+++ app/Models/Label.php
@@ -10,4 +10,12 @@ class Label extends Model
     use HasFactory;
 
     protected $fillable = ['name', 'description'];
+
+    /**
+     * Get all the tasks related to the label.
+     */
+    public function tasks(): BelongsToMany
+    {
+        return $this->belongsToMany(Task::class);
+    }
 }
