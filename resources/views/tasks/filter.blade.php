670e5eb99752a1458c8c1369c70de4d17ac8911d
Николай <rnn2002@mail.ru>
2023-11-09 11:56:44 +0300

    Modify tasks/filter view to fix some tests

--- resources/views/tasks/filter.blade.php
+++ resources/views/tasks/filter.blade.php
@@ -1,8 +1,8 @@
 <div class="w-full flex items-center">
     {{ Form::model($task, ['route' => ['tasks.index', $tasks], 'method' => 'GET']) }}
-    {{ Form::select('filter[status_id]', $taskStatuses, $filter['status_id'], ['placeholder' => 'Статус', 'class' => 'rounded border-blue-300 mb-1']) }}
-    {{ Form::select('filter[created_by_id]', $users, $filter['created_by_id'], ['placeholder' => 'Автор', 'class' => 'rounded border-blue-300 mb-1']) }}
-    {{ Form::select('filter[assigned_to_id]', $users, $filter['assigned_to_id'], ['placeholder' => 'Исполнитель', 'class' => 'rounded border-blue-300 mb-1']) }}
+    {{ Form::select('filter[status_id]', $taskStatuses, $filter['status_id'] ?? '', ['placeholder' => 'Статус', 'class' => 'rounded border-blue-300 mb-1']) }}
+    {{ Form::select('filter[created_by_id]', $users, $filter['created_by_id'] ?? '', ['placeholder' => 'Автор', 'class' => 'rounded border-blue-300 mb-1']) }}
+    {{ Form::select('filter[assigned_to_id]', $users, $filter['assigned_to_id'] ?? '', ['placeholder' => 'Исполнитель', 'class' => 'rounded border-blue-300 mb-1']) }}
     {{ Form::submit('Применить', ['class' => 'bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded']) }}
     {{ Form::close() }}
 </div>
