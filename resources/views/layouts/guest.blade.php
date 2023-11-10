126077b48dccd6c83c70fa4effee323789c6fa1b
Николай <rnn2002@mail.ru>
2023-11-10 19:18:43 +0300

    Add layouts/guest localization

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -18,7 +18,7 @@
         <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
             <div class="text-4xl">
                 <a href="/">
-                    Менеджер задач
+                    {{ __('guest.app_name') }}
                 </a>
             </div>
 
