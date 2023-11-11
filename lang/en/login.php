3be4f0c1bf93b26378e26dc8b0c05a2b8a59cc42
Николай <rnn2002@mail.ru>
2023-11-11 19:56:33 +0300

    Add auth/login localization

---
+++ lang/en/login.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+
+    'email' => 'Email',
+    'password' => 'Password',
+    'remember_me' => 'Remember me',
+    'forgot_password' => 'Forgot your password?',
+    'log_in' => 'Log in',
+
+];
---
+++ lang/ru/login.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+
+    'email' => 'Email',
+    'password' => 'Пароль',
+    'remember_me' => 'Запомнить меня',
+    'forgot_password' => 'Забыли пароль?',
+    'log_in' => 'Войти',
+
+];
