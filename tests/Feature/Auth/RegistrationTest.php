50e9cf6cc7d07ac73bbc30c21fb1273909eb0613
Николай <rnn2002@mail.ru>
2023-11-12 13:56:35 +0300

    Modify RegistrationTest

--- tests/Feature/Auth/RegistrationTest.php
+++ tests/Feature/Auth/RegistrationTest.php
@@ -13,8 +13,9 @@ class RegistrationTest extends TestCase
     public function test_registration_screen_can_be_rendered(): void
     {
         $response = $this->get('/register');
-
         $response->assertStatus(200);
+
+        $response = $this->get(route('register'))->assertSee( __('register.register'));
     }
 
     public function test_new_users_can_register(): void
